fcf777fedea6e14a998db91fdc74cbdf7558561e
40grivenprog <grivenmus40@yandex.ru>
2020-07-23 19:54:46 +0300

    Fix notes

--- app.rb
+++ app.rb
@@ -5,20 +5,30 @@ class App
 
   def call(env)
     response = Rack::Response.new
-    response.status = status(env)
-    time_formatter = TimeFormatter.new(env['QUERY_STRING'])
-    response.write(time_formatter.call)
+    if correct_path?(env['REQUEST_PATH'])
+      time_formatter = TimeFormatter.new(env['QUERY_STRING'])
+      response.write(time_formatter.call)
+      response.status = time_formatter.success ? 200 : 400
+    else
+      response.status = 404
+      response.write("Incorrect path")
+    end
     response['Content-Type'] = "text/plain"
     response.finish
   end
 
   private
 
+  def correct_path?(path)
+    path == '/time'
+  end
+
   def status(env)
-    return 404 if env['REQUEST_PATH'] != '/time'
-    return 400 if env['QUERY_STRING'].empty?
-    return 400 unless (env['QUERY_STRING'][/format=(.*)/, 1].split('%2C') - TimeFormatter::AVAILIABLE_PARAMS.keys).empty?
-    200
+    # return 404 if env['REQUEST_PATH'] != '/time'
+    # return 400 if env['QUERY_STRING'].empty?
+    # return 400 unless (env['QUERY_STRING'][/format=(.*)/, 1].split('%2C') - TimeFormatter::AVAILIABLE_PARAMS.keys).empty?
+    # 200
+    # env['REQUEST_PATH'] == '/time' ? 200 : 404
   end
 
   def headers
